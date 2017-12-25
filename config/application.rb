c5b9431714a469518207f7dc2cf77173f5a0f624
Vikki <vika.malakutsko@gmail.com>
2017-12-25 21:26:40 +0100

    Queue emails in sidekiq

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -14,5 +16,7 @@ module MoviesApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
